ccb271ca21bf742325b29626468ddf8b7f61a150
Emily warburton <69345974+Ewarbs@users.noreply.github.com>
2021-01-06 19:00:13 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       Cat facts
+      <footer>
+        This project was coded by Emily Warburton and is {" "}
+      <a href="https://github.com/Ewarbs/cat_facts" target="_blank">
+open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
